2f40050f495f5625d23e38369c05ddf67daccd25
moazam <moazamshahzad222@gmail.com>
2021-02-12 12:11:59 -0500

    AWs regions has been added and tfstate file and .gitignore files have been added

--- remote_state.tf
+++ remote_state.tf
@@ -1,11 +1,14 @@
+provider "aws" {
+  region = var.region
+}
 resource "aws_s3_bucket" "remote_state" {
-bucket = "${var.prefix}-remote_state-${var.environment}"
+bucket = "${var.prefix}-remote_state-${var.enviornment}"
 acl = "authenticated-read"
 versioning {
 enabled = true
 }
-tags {
-Name = "${var.prefix}-remote_state-${var.environment}"
-Environment = "${var.environment}"
+tags ={
+Name = "${var.prefix}-remote_state-${var.enviornment}"
+Environment = "${var.enviornment}"
 }
 }
--- remote_stateinterface.tf
+++ remote_stateinterface.tf
@@ -3,10 +3,10 @@ default = "us-east-1"
 description = "The AWS region."
 }
 variable "prefix" {
-default = "examplecom"
-description = "The name of our org, i.e. examplecom."
+default = "terraform"
+description = "it can be name or anything to make s3 bucket's name more unique."
 }
-variable "environment" {
-default = "development"
-description = "The name of our environment, i.e. development."
+variable "enviornment" {
+default = "Book_project"
+description = "The name of our environment, i.e. Book_project."
 }
